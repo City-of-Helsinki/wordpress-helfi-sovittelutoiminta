9082199b23b8d13508a688de1ddf46b1c9615634
Henri Tukiainen <henri@momocode.com>
2021-02-01 00:23:53 +0200

    Update category and tag page

--- web/app/themes/helsinki-sovittelutoiminta/resources/views/partials/page-header.blade.php
+++ web/app/themes/helsinki-sovittelutoiminta/resources/views/partials/page-header.blade.php
@@ -1,6 +1,10 @@
 <header class="entry-header">
-  @if (!is_front_page() && function_exists('yoast_breadcrumb'))
-    {{ yoast_breadcrumb( '<p id="breadcrumbs">','</p>' ) }}
-  @endif
-  <h1 class="entry-title">{!! $title !!}</h1>
+  <x-group align="full" background="silver-medium-light">
+    @if (function_exists('yoast_breadcrumb'))
+      {{ yoast_breadcrumb( '<p id="breadcrumbs" class="alignwide">','</p>' ) }}
+    @endif
+    <h1 class="entry-title">{!! $title !!}</h1>
+  </x-group>
+  <div class="koro koro--pulse white bottom desktop-koro">
+  </div>
 </header>
