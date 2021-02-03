b223f2b468eea1ee23b261dc8f50d32270a2ffa1
Henri Tukiainen <henri@momocode.com>
2021-02-04 01:24:55 +0200

    Remove related content from posts

--- web/app/themes/helsinki-sovittelutoiminta/resources/views/partials/content-single.blade.php
+++ web/app/themes/helsinki-sovittelutoiminta/resources/views/partials/content-single.blade.php
@@ -49,15 +49,4 @@
       @include('partials/entry-meta')
     </div>
   </div>
-
-  <x-related-content
-    :type="$related->type"
-    :label="$related->label"
-    :query="$related->query"
-    :category="$related->category"
-  />
-
-  <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-  </footer>
 </article>
