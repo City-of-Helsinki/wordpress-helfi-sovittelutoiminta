3465d31bd6aabb244090ffb568e0a460460b6d9b
Henri Tukiainen <henri@momocode.com>
2021-02-14 04:21:29 +0200

    Add schema.org support to question and answer block

--- web/app/themes/helsinki-sovittelutoiminta/resources/views/blocks/question-and-answer.blade.php
+++ web/app/themes/helsinki-sovittelutoiminta/resources/views/blocks/question-and-answer.blade.php
@@ -1,8 +1,23 @@
-<div class="{{ esc_attr($block->classes) }}">
-  <div class="wp-block-hds-question-and-answer__question">
+<div
+  class="{{ esc_attr($block->classes) }}"
+  itemscope
+  itemprop="mainEntity"
+  itemtype="https://schema.org/Question"
+>
+  <div
+    class="wp-block-hds-question-and-answer__question"
+    itemprop="name"
+  >
     {{ $question }}
   </div>
-  <div class="wp-block-hds-question-and-answer__answer">
-    {!! $content !!}
+  <div
+    class="wp-block-hds-question-and-answer__answer"
+    itemscope
+    itemprop="acceptedAnswer"
+    itemtype="https://schema.org/Answer"
+  >
+    <div itemprop="text">
+      {!! $content !!}
+    </div>
   </div>
 </div>
